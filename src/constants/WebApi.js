8bba0196961b81548e5b0805424a2f94f61412e8
Roman Bondarenko <bask-5@yandex.ru>
2018-01-23 09:24:31 +0300

    7-define-recaptcha-sitekey-as-environment-variable: Fixed env to prod

--- src/constants/WebApi.js
+++ src/constants/WebApi.js
@@ -1,5 +1,5 @@
-// export const WEB_API = process.env.REACT_APP_API || 'https://web.api.containerum.io:5000';
-// export const WEB_API_OTHER = process.env.REACT_APP_OTHER || 'https://web.api.containerum.io:5000';
-export const WEB_API = 'http://192.168.88.200:5000';
-export const WEB_API_OTHER = 'http://192.168.88.200:5000';
+export const WEB_API = process.env.REACT_APP_API || 'https://web.api.containerum.io:5000';
+export const WEB_API_OTHER = process.env.REACT_APP_OTHER || 'https://web.api.containerum.io:5000';
+// export const WEB_API = 'http://192.168.88.200:5000';
+// export const WEB_API_OTHER = 'http://192.168.88.200:5000';
 export const APP_RECAPTCHA = process.env.REACT_APP_RECAPTCHA || '6LejdSMUAAAAADNv4yBEqxz4TAyXEIYCbwphVSDS';
