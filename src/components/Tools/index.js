2bfa55423c7640d9774fb0e4bbcf8914c33c0f2a
ngorin <nikitagn9@gmail.com>
2018-04-18 16:22:24 +0300

    60-fixed-markup-on-tools-page:fixed markup on tools page

--- src/components/Tools/index.js
+++ src/components/Tools/index.js
@@ -7,7 +7,7 @@ const Tools = () => (
     <Helmet title="Tools" />
     <div className="container container__tools no-back">
       <Link to="/configmap">
-        <div className="col-md-4 tools__card">
+        <div className="col-md-12 tools__card">
           <div className="content-block-container
         content-block-container__tools
         card-container__tools
@@ -28,51 +28,15 @@ const Tools = () => (
               content-block__info-name_tools
                content-block-header-label__text_center">
                   ConfigMaps allow you to decouple configuration artifacts from
-                  image <br /> content to keep containerized applications
-                  portable.
+                  image content to keep containerized applications portable.
                 </div>
               </div>
             </div>
           </div>
         </div>
       </Link>
-      <div>
-        {/* Link to="/webhook" */}
-        <div className="col-md-4 tools__card_center">
-          <div
-            style={{ backgroundColor: '#fafafa' }}
-            className="content-block-container content-block-container__tools content-block-container__tools-hover-always card-container card-container__tools card-container__tools_not-active hover-action-always"
-          >
-            <div className="content-block-header ">
-              <div className="content-block-header-label
-                         content-block-header-label__tools">
-                <div className="content-block-header-img__tools content-block-header-img__tools_webhooks" />
-                <div className="content-block-header-label_main">
-                  CD WEBHOOKS
-                </div>
-              </div>
-            </div>
-
-            <div className="content-block-content
-          card-block
-          content-block-content__tools ">
-              <div className="content-block__info-item ">
-                <div className="content-block__info-name
-              content-block__info-name_tools
-               content-block-header-label__text_center">
-                  CCD WebHooks allow you to automate updating images in
-                  containers.
-                  <br />
-                  <br />
-                  <span style={{ color: '#29abe2' }}>Coming soon</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <Link to="/domains">
-        <div className="col-md-4 tools__card_right">
+        <div className="col-md-12 tools__card">
           <div className="content-block-container
         content-block-container__tools
         card-container card-container__tools
@@ -99,6 +63,41 @@ const Tools = () => (
           </div>
         </div>
       </Link>
+      <div>
+        {/* Link to="/webhook" */}
+        <div className="col-md-12 tools__card">
+          <div
+            style={{ backgroundColor: '#fafafa' }}
+            className="content-block-container content-block-container__tools content-block-container__tools-hover-always card-container card-container__tools card-container__tools_not-active hover-action-always"
+          >
+            <div className="content-block-header ">
+              <div className="content-block-header-label
+                         content-block-header-label__tools">
+                <div className="content-block-header-img__tools content-block-header-img__tools_webhooks" />
+                <div className="content-block-header-label_main">
+                  CD WEBHOOKS
+                </div>
+              </div>
+            </div>
+
+            <div className="content-block-content
+          card-block
+          content-block-content__tools ">
+              <div className="content-block__info-item ">
+                <div className="content-block__info-name
+              content-block__info-name_tools content-block__info-name_tools-wb
+               content-block-header-label__text_center">
+                  CCD WebHooks allow you to automate updating images in
+                  containers.
+                  <br />
+                  <br />
+                  <span style={{ color: '#29abe2' }}>Coming soon</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
