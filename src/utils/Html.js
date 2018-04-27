6f3599597a61edbbf0463dc4e536d62e1099d14e
ngorin <nikitagn9@gmail.com>
2018-04-27 13:48:25 +0300

    79-transfer-styles-in-scss-on-tools-page:transfered syles in scss on tools page

--- src/utils/Html.js
+++ src/utils/Html.js
@@ -155,6 +155,7 @@ const Html = ({ store, htmlContent }: Props): Element<'html'> => {
                 require('../theme/individual.css')._style +
                 require('../theme/style.css')._style +
                 require('../theme/style-custom.css')._style +
+                require('../theme/common.scss')._style +
                 require('../containers/Support/Support.css')._style +
                 require('../components/ProfileInfo/Profile.css')._style +
                 require('../containers/Account/Account.css')._style
