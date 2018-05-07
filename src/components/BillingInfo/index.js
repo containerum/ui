11882f176941250c68e6ac4402bcfa932723adff
ngorin <nikitagn9@gmail.com>
2018-05-07 13:36:55 +0300

    94-transfer-styles-in-scss-on-biling-page:transfer styles in scss on billing, without AddModal, buttons and pagination

--- src/components/BillingInfo/index.js
+++ src/components/BillingInfo/index.js
@@ -2,6 +2,9 @@
 
 import React from 'react';
 
+import globalStyles from '../../theme/global.scss';
+import billingStyles from '../../containers/Billing/index.scss';
+
 type Props = {
   statusUser: string,
   formatDateToActive: string,
@@ -17,12 +20,12 @@ const BillingInfo = ({
   monthUsage,
   dailyUsage
 }: Props) => (
-  <div className="block-item" id="information">
-    <div className="block-item__title">Information</div>
-    <div className="row billing-content-row">
+  <div className={globalStyles.blockItem} id="information">
+    <div className={globalStyles.blockItemTitle}>Information</div>
+    <div className="row">
       <div className="col-md-4">
-        <div className="billing-content-text inline">Status:&nbsp;</div>
-        <div className="billing-information-status-info inline">
+        <div className={billingStyles.billingContentText}>Status:&nbsp;</div>
+        <div className={billingStyles.billingInformationStatusInfo}>
           {statusUser}
         </div>
       </div>
@@ -30,25 +33,31 @@ const BillingInfo = ({
       <div className="col-md-8">
         <div className="row">
           <div className="col-md-3">
-            <div className="billing-information-data">Balance</div>
-            <div className="billing-information-data">Month usage</div>
-            <div className="billing-information-data">Daily usage</div>
+            <div className={billingStyles.billingInformationData}>Balance</div>
+            <div className={billingStyles.billingInformationData}>
+              Month usage
+            </div>
+            <div className={billingStyles.billingInformationData}>
+              Daily usage
+            </div>
             {formatDateToActive && (
-              <div className="billing-information-data">Paid up to</div>
+              <div className={billingStyles.billingInformationData}>
+                Paid up to
+              </div>
             )}
           </div>
           <div className="col-md-9">
-            <div className="billing-information-data fw-normal">
+            <div className={billingStyles.billingInformationData}>
               ${balance ? balance.toFixed(2) : 0}
             </div>
-            <div className="billing-information-data">
+            <div className={billingStyles.billingInformationData}>
               ${monthUsage ? monthUsage.toFixed(2) : 0}
             </div>
-            <div className="billing-information-data">
+            <div className={billingStyles.billingInformationData}>
               ${dailyUsage ? dailyUsage.toFixed(2) : 0}
             </div>
             {formatDateToActive && (
-              <div className="billing-information-data">
+              <div className={billingStyles.billingInformationData}>
                 {formatDateToActive}
               </div>
             )}
